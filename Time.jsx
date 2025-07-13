5042e3fb6f11495d153d3d1a9c1c16fb10b384bd
dharaniravi08 <dharanir283@gmail.com>
2025-07-13 19:17:10 +0530

    Add files via upload

---
+++ Time.jsx
@@ -0,0 +1,38 @@
+ import React from "react";
+ import { useState } from "react";
+
+import './Time.css'
+ function Time() {
+    const [Count, setCount] = useState(0)
+  return (
+    <>
+      <div className="bg">
+      <div className="main">
+          <div className="but">
+            <h1 > COUNTER</h1>
+         <div className="bt1"> &#128336;<button onClick={() => setCount(Count+1)}>Increase</button></div>
+         <div className="bt2">&#128336;<button onClick={()=>setCount(Count-1)}>Decrease</button></div>
+         <div className="bt3"> &#128336;<button onClick={() => setCount(0)}>Reset</button></div>
+        <h1 className="hea">{Count}</h1>
+        </div></div></div>
+      </>
+  )
+ }
+
+ export default Time
+
+
+ 
+//  function Time() {
+//     const [Count, setCount] = useState()
+//    return (
+//        <>
+//            <button onClick={() => setCount("Hi")}>Show</button>
+//         <button onClick={()=>setCount(" ")}>Hide</button>
+//            <h1>{Count}</h1>   
+       
+//        </>
+//    )
+//  }
+ 
+//  export default Time
